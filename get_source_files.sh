f53600d77df90192bf5906fe74a1c5f00fa414cd
Agustin Pardo <agu_pardo@hotmail.com>
2022-04-29 17:29:44 -0300

    Model parser as object oriented

--- get_source_files.sh
+++ get_source_files.sh
@@ -1,3 +1,14 @@
-wget --no-parent --level=1 --no-directories --directory-prefix=/evidence --accept=*.json -r ftp://ftp.ebi.ac.uk/pub/databases/opentargets/platform/21.11/output/etl/json/evidence/sourceId=eva/
-wget --no-parent --level=1 --no-directories --directory-prefix=/diseases --accept=*.json -r ftp://ftp.ebi.ac.uk/pub/databases/opentargets/platform/21.11/output/etl/json/diseases/
-wget --no-parent --level=1 --no-directories --directory-prefix=/targets --accept=*.json -r ftp://ftp.ebi.ac.uk/pub/databases/opentargets/platform/21.11/output/etl/json/targets/
+# Create directories
+data_dir=data
+mkdir -p $data_dir/{evidence,targets,diseases}
+
+# Get Data sets on parallel
+wget --no-parent --level=1 --no-directories --directory-prefix=$data_dir/evidence --accept='*.json' -r ftp://ftp.ebi.ac.uk/pub/databases/opentargets/platform/21.11/output/etl/json/evidence/sourceId=eva/ -b
+wget --no-parent --level=1 --no-directories --directory-prefix=$data_dir/diseases --accept='*.json' -r ftp://ftp.ebi.ac.uk/pub/databases/opentargets/platform/21.11/output/etl/json/diseases/ -b
+wget --no-parent --level=1 --no-directories --directory-prefix=$data_dir/targets --accept='*.json' -r ftp://ftp.ebi.ac.uk/pub/databases/opentargets/platform/21.11/output/etl/json/targets/ -b
+
+# Checking wget log for errors
+grep -e "error" -e "fail" wget-log wget-log.1 wget-log.2 | awk -F: '{print "Line "$1": "$2}'
+
+# Check donwload finish
+grep -e "FINISHED"  wget-log wget-log.1 wget-log.2
--- parser.py
+++ parser.py
@@ -1,124 +1,122 @@
 import os
-import time
 import pandas as pd
 import json
 import math
 from multiprocessing import Process, Queue, cpu_count
 
-def get_data(path, files, queue, *args):
-    list_for_df = []
-    for file in files:
-        file=open(path+"/"+file)
-        readlines = file.readlines()
-        file.close()
-        for line in readlines:
-            dict_to_list = json.loads(line)
-            # Keep only args requested data
-            dict_to_list = {x:dict_to_list[x] for x in args}
-            list_for_df.append(dict_to_list)
+class DataSet:
 
-    # Add result to queue
-    queue.put(list_for_df)
+    def __init__(self, path, name, fields):
+        self.path = path+"/"+name
+        self.name = name
+        self.fields = fields
+        self.df = None
 
-def parallel_get_data(path, files, *args):
-    # Set number of CPUs
-    # num_workers = cpu_count()
-    num_cpus = 6
+class Parser:
 
-    # Set a Queue to store each process result
-    queue = Queue()
+    def __init__(self, data_path):
+        self.path = data_path
+        self.evidence = DataSet(self.path, "evidence", ["targetId", "diseaseId", "score"])
+        self.target = DataSet(self.path, "targets", ["id", "approvedSymbol"])
+        self.disease = DataSet(self.path, "diseases", ["id", "name"])
 
-    # Set chunks to distributed over CPUs number
-    num_files=len(files)
-    chunk_step=math.ceil(num_files/num_cpus)
+    def get_data(self, path, files, queue, args):
+        list_for_df = []
+        for file in files:
+            file=open(path+"/"+file)
+            readlines = file.readlines()
+            file.close()
+            for line in readlines:
+                dict_to_list = json.loads(line)
+                # Keep only args requested data
+                dict_to_list = {x:dict_to_list[x] for x in args}
+                list_for_df.append(dict_to_list)
 
-    for w in range(num_cpus):
-        left=chunk_step*w
-        right=chunk_step*(w+1)
-        if right>num_files:
-            right=num_files
-        process = Process(target=get_data, args=(path, files[left:right], queue, *args))
-        process.start()
-        right = left
+        # Add result to queue
+        queue.put(list_for_df)
 
-    # Collect the queue results
-    all_results = []
-    for _ in range(0, num_cpus):
-        all_results+=queue.get()
+    def parallel_get_data(self, path, files, args):
+        # Set number of CPUs
+        # num_workers = cpu_count()
+        num_cpus = 6
 
-    return all_results
+        # Set a Queue to store each process result
+        queue = Queue()
 
-def parse_data(files_path, *args):
+        # Set chunks to distributed over CPUs number
+        num_files=len(files)
+        chunk_step=math.ceil(num_files/num_cpus)
 
-    files = os.listdir(files_path)
-    data = parallel_get_data(files_path, files, *args)
-    dataframe = pd.DataFrame(data)
+        for w in range(num_cpus):
+            left=chunk_step*w
+            right=chunk_step*(w+1)
+            if right>num_files:
+                right=num_files
+            process = Process(target=self.get_data, args=(path, files[left:right], queue, args))
+            process.start()
+            right = left
 
-    return dataframe
+        # Collect the queue results
+        all_results = []
+        for _ in range(0, num_cpus):
+            all_results+=queue.get()
 
+        return all_results
 
-def tranform_data():
-    evidence_df = parse_data("data/evidence/", "targetId", "diseaseId", "score")
-    target_df = parse_data("data/targets/", "id", "approvedSymbol")
-    diseases_df = parse_data("data/diseases/", "id", "name")
+    def parse_data(self, dataset):
+        files = os.listdir(dataset.path)
+        data = self.parallel_get_data(dataset.path, files, dataset.fields)
+        dataset.df = pd.DataFrame(data)
 
-    # Calculate median score and 3 top scores per targetId and diseaseId pair
-    print("Calculating Median and 3 top scores")
-    evidence_df = evidence_df.groupby(["targetId", "diseaseId"]).agg({"score": ['mean', lambda s: sorted(list(s), reverse=True)[:3]]}).reset_index()
-    columns_name =("targetId", 'diseaseId', 'median', 'top3')
-    evidence_df.columns = list(map(''.join, columns_name))
+    def prepare_evidence(self, dataset):
 
-    print("Joining evidence, target and disease dataset")
-    # Join with target
-    ev_ta_df = pd.merge(evidence_df, target_df, left_on="targetId", right_on="id")
-    # Join with disease
-    ev_ta_di_df = pd.merge(ev_ta_df, diseases_df, left_on="diseaseId", right_on="id")
-    # Remove no usable columns
-    del ev_ta_di_df["id_x"]
-    del ev_ta_di_df["id_y"]
+        self.parse_data(dataset)
+        # Calculate median score and 3 top scores per targetId and diseaseId pair
+        print("Calculating median and 3 top scores of evidence data set")
+        dataset.df = dataset.df.groupby(["targetId", "diseaseId"]).agg({"score": ['mean', lambda s: sorted(list(s), reverse=True)[:3]]}).reset_index()
+        columns_name =("targetId", 'diseaseId', 'median', 'top3')
+        dataset.df.columns = list(map(''.join, columns_name))
 
-    # Sort ascending on median
-    print("Sorting by median score")
-    ev_ta_di_df = ev_ta_di_df.sort_values("median").reset_index(drop=True)
+    def transform_data(self):
+        self.prepare_evidence(self.evidence)
+        self.parse_data(self.target)
+        self.parse_data(self.disease)
 
-    return ev_ta_di_df
+        print("Joining evidence, target and disease dataset")
+        # Join with target
+        ev_ta_df = pd.merge(self.evidence.df, self.target.df, left_on="targetId", right_on="id")
+        # Join with disease
+        ev_ta_di_df = pd.merge(ev_ta_df, self.disease.df, left_on="diseaseId", right_on="id")
+        # Remove no usable columns
+        del ev_ta_di_df["id_x"]
+        del ev_ta_di_df["id_y"]
 
-def export_data(df, out_file):
-    print(f"Exporting Results to JSON file: {out_file}")
-    df.to_json(out_file, orient="records")
-    print(f"Data exported to {out_file}")
+        # Sort ascending on median
+        print("Sorting by median score")
+        self.data_transformed = ev_ta_di_df.sort_values("median").reset_index(drop=True)
 
-def target_target_pair(df):
-    df1 = df
-    df2 = df
-    # Join on targetID
-    tt_pair = pd.merge(df1, df2, left_on="targetId", right_on="targetId")
+    def export_data(self, out_file):
+        print(f"Exporting Results to JSON file: {out_file}")
+        self.data_transformed.to_json(out_file, orient="records")
+        print(f"Data exported to {out_file}")
 
-    # Remove all the rows having same diseasesID
-    tt_pair_dif_diseases = tt_pair[tt_pair["diseaseId_x"] != tt_pair["diseaseId_y"]]
+    def target_target_pair(self):
 
-    tt_pair_count = tt_pair_dif_diseases["targetId"].count() / 2
-    print(f"Target-Target pair sharing connection with atleast two diseases {int(tt_pair_count)}")
+        if self.evidence.df.empty:
+            self.prepare_evidence(self.evidence)
+
+        # Join on targetID
+        tt_pair = pd.merge(self.evidence.df, self.evidence.df, left_on="targetId", right_on="targetId")
+
+        # Remove all the rows having same diseasesID
+        tt_pair_dif_diseases = tt_pair[tt_pair["diseaseId_x"] != tt_pair["diseaseId_y"]]
+
+        tt_pair_count = tt_pair_dif_diseases["targetId"].count() / 2
+        print(f"Target-Target pair sharing connection with atleast two diseases {int(tt_pair_count)}")
 
 if __name__ == "__main__":
-    # ARG_PARSER = argparse.ArgumentParser()
-    # ARG_PARSER.add_argument(
-    #     "--datadir",
-    #     help="Directory where evidence, targets and diseases are stored",
-    #     default="data",
-    # )
-    # ARG_PARSER.add_argument(
-    #     "--outfile",
-    #     help="The name of the output file where results will be stored",
-    #     default="evidence_mt3.json",    )
 
-    # CLI_ARGS = ARG_PARSER.parse_args()
-    # data_dir = CLI_ARGS.datadir
-    # outfile = CLI_ARGS.outfile
-
-    # print(data_dir, outfile)
-
-    df = tranform_data()
-    export_data(df, "output.json")
-    target_target_pair(df)
-    
+    parser = Parser("data")
+    parser.transform_data()
+    parser.export_data('output.json')
+    parser.target_target_pair()
