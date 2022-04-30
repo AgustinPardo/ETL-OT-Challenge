b5bd5dd80e9375279b1207448e9b4cae8a05c6c6
Agustin Pardo <agu_pardo@hotmail.com>
2022-04-30 14:52:27 -0300

    Add main to manage app

---
+++ main.py
@@ -0,0 +1,28 @@
+import argparse
+from parser import Parser, CPU
+
+def parse_arguments():
+    """Command line arguments parser"""
+    parser = argparse.ArgumentParser(description="ETL pipeline")
+    parser.add_argument("-in", "--input_dir", default="data", help="Evidence, targets, diseases, data sets input directory")
+    parser.add_argument("-out", "--output_file", default="parse_evidence.json", help="Reuslt JSON file output name")
+    parser.add_argument("-n", "--cpus_use", default=1, help="Numbers of CPUs used to parse data sets. Deaults is 1")
+    parser.add_argument("-nc", action='store_true', help="Get the number of CPUs available")
+    parser.add_argument('-etl', action='store_true', help="Run ETL pipeline")
+    parser.add_argument('-tt', action='store_true', help="Run target-target calculation")
+    return parser
+
+def main():
+    parser=parse_arguments()
+    args=parser.parse_args()
+    parser = Parser(args.input_dir, args.cpus_use)
+    if args.nc:
+        CPU.get_avaiable_cpus()
+    if args.etl:
+        parser.transform_data()
+        parser.export_data(args.output_file)
+    if args.tt:
+        parser.target_target_pair()
+
+if __name__ == "__main__":
+    main()
